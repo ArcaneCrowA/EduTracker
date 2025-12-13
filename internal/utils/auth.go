57795bd966f3d2eeb3ceab168ee2f771cb79fdc7
Adilet <adiletbeishekeyev@gmail.com>
2025-12-13 15:44:49 +0500

    Update auth.go

--- internal/utils/auth.go
+++ internal/utils/auth.go
@@ -2,12 +2,13 @@ package utils
 
 import (
 	"errors"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
-var jwtKey = []byte("my_secret_key")
+var jwtKey = []byte(os.Getenv("SECRET_KEY"))
 var blacklist = make(map[string]bool)
 
 type Claims struct {
@@ -29,45 +30,28 @@ func GenerateToken(userID uint) (string, error) {
 }
 
 func ValidateToken(tokenString string) (*Claims, error) {
-
 	if IsTokenBlacklisted(tokenString) {
-
 		return nil, errors.New("token is blacklisted")
-
 	}
 
 	claims := &Claims{}
-
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
-
 		return jwtKey, nil
-
 	})
-
 	if err != nil {
-
 		return nil, err
-
 	}
-
 	if !token.Valid {
-
 		return nil, err
-
 	}
-
 	return claims, nil
 
 }
 
 func BlacklistToken(tokenString string) {
-
 	blacklist[tokenString] = true
-
 }
 
 func IsTokenBlacklisted(tokenString string) bool {
-
 	return blacklist[tokenString]
-
 }
