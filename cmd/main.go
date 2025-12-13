815fe7eed84bd9ae426918b488bb9ab2e3125607
Adilet <adiletbeishekeyev@gmail.com>
2025-12-13 16:08:47 +0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/arcanecrowa/EduTracker/internal/routes"
 	database "github.com/arcanecrowa/EduTracker/pkg/db"
 )
@@ -8,5 +10,5 @@ import (
 func main() {
 	database.InitDB()
 	r := routes.SetupRouter()
-	r.Run(":8080")
+	log.Fatal(r.Run(":8080"))
 }
